b809866302fe69b2b7e9f4797499506820c602c0
Jonathan Kene <Q3erty7>
2018-10-19 14:59:36 -0400

    Update NumericTextFieldsJonathan/main.lua

--- NumericTextFieldsJonathan/main.lua
+++ NumericTextFieldsJonathan/main.lua
@@ -12,17 +12,25 @@ display.setStatusBar(display.HiddenStatusBar)
 -- sets the background colour
 display.setDefault("background", 124/255, 249/255, 199/255)
 
+---------------------------------------------------
+--LOCAL VARIABLES
+-------------------------------------------------
+
 local questionObject
-local questionObject2
+
 local correctObject
 local incorrectObject
+
 local numericField
+
 local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
+
 local pointsTextObject
 local numberPoints = 0
+
 local randomOperator
 local gameOver
 
@@ -32,9 +40,17 @@ local secondsLeft = 5
 local clockText
 local countDownTimer
 
-local lives = 2
+local lives = 4
 local heart1
 local heart2
+local heart3
+local heart4
+
+
+---------------------------------------------------
+-- SOUNDS
+-------------------------------------------------
+
 
 -- Correct sound
 local correctSound = audio.loadSound( "Sounds/correctSound.mp3" ) -- Setting a variable to an mp3 file
@@ -43,10 +59,31 @@ local wrongSound = audio.loadSound( "Sounds/wrongSound.mp3" ) -- Setting a varia
 local correctSoundChannel
 local wrongSoundChannel
 
+ 
+
+---------------------------------------------------
+--LOCAL FUNCTIONS
+-------------------------------------------------
+
+local function UpdateLives()
+	if (lives == 3) then
+    	heart4.isVisible = false	
+    elseif (lives == 2) then
+        heart3.isVisible = false
+    elseif (lives == 1) then
+        heart2.isVisible = false
+    elseif (lives == 0) then
+    	heart1.isVisible = false
+        gameOver.isVisible = true
+        timer.cancel(countDownTimer )
+        numericField.isVisible = false
+    end 
+end
+
 local function UpdateTime()
 
 	-- decrement the number of seconds left
-	secondsLeft = secondsLeft - 1
+	secondsLeft = secondsLeft -1
 
 	-- display the number of seconds left in the clock object
 	clockText.text = secondsLeft .. ""
@@ -56,15 +93,7 @@ local function UpdateTime()
     	secondsLeft = totalSeconds
     	lives = lives - 1
 
-    	if (lives == 2) then
-    		heart2.isVisible = false
-    		gameOver.isVisible = false
-        elseif (lives == 1) then
-        	heart1.isVisible = false
-        	gameOver.isVisible = false
-        elseif (lives == 0) then
-            gameOver.isVisible = true
-        end
+    	UpdateLives()
     end
 end
 
@@ -73,19 +102,7 @@ local function StartTimer()
 	-- create countdown timer that loops infinitely
 	countDownTimer = timer.performWithDelay(1000, UpdateTime, 0)
 end
- 
-local function UpdateLives()
-	if (lives == 2) then
-    		heart2.isVisible = false
-    		gameOver.isVisible = false
-        elseif (lives == 1) then
-        	heart1.isVisible = false
-        	gameOver.isVisible = false
-        elseif (lives == 0) then
-            gameOver.isVisible = true
-        end
-    
-end
+
 
  local function AskQuestion()
 	-- generate 2 random numbers between a max. and a min. number
@@ -153,7 +170,8 @@ local function NumericFieldListener(event)
 	    	wrongSoundChannel = audio.play(wrongSound)
 	    	timer.performWithDelay(2000, hideIncorrect)
 	    	lives = lives - 1
-	    	-- call the function to decrease lives
+
+	    -- call the function to decrease lives
 	    	UpdateLives()
 	    end
 
@@ -161,6 +179,10 @@ local function NumericFieldListener(event)
 
 	end
 end
+
+---------------------------------------------------
+-- OBJECT CREATION
+-------------------------------------------------
 
 -- create points box adn make it visible
 pointsTextObject = display.newText( "Points = ".. numberPoints, 800, 385, nil, 50 )
@@ -181,6 +203,13 @@ incorrectObject = display.newText( "Incorrect!", display.contentWidth/2, display
 incorrectObject:setTextColor(155/255, 42/255, 198/255)
 incorrectObject.isVisible = false
 
+-- create the incorrect text object and make it invisble
+clockText = display.newText( "", display.contentWidth/2, display.contentHeight*2/3, nil, 100 )
+clockText:setTextColor(155/255, 42/255, 198/255)
+clockText.isVisible = true
+clockText.x = 200
+clockText.y = 600
+
 -- Create numeric field
 numericField = native.newTextField( 520, 380, 150, 90 )
 numericField.inputType = "number"
@@ -189,7 +218,7 @@ numericField.inputType = "number"
 numericField:addEventListener( "userInput", NumericFieldListener )
 
 -- add background image
-local gameOver = display.newImageRect("Images/gameOver.png", 1350, 900)
+gameOver = display.newImageRect("Images/gameOver.png", 1350, 900)
 gameOver.x = 520
 gameOver.y = 348
 gameOver.isVisible = false
@@ -202,6 +231,21 @@ heart1.y = display.contentHeight * 1 / 7
 heart2 = display.newImageRect("Images/heart.png", 100, 100)
 heart2.x = display.contentWidth * 6 / 8
 heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3.x = display.contentWidth * 6 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect("Images/heart.png", 100, 100)
+heart4.x = display.contentWidth * 6 / 8
+heart4.y = display.contentHeight * 1 / 7
+
+---------------------------------------------------
+-- FUNCTION CALLS
+-------------------------------------------------
+
+-- call the function to ask the question
+StartTimer()
 
 -- call the function to ask the question
 AskQuestion()
