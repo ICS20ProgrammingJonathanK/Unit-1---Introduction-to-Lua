20f53b0c425e8e238d248e3db53f1cf8f7b1e304
Jonathan Kene <Q3erty7>
2018-10-25 13:23:12 -0400

    Update Numeric Text fields

--- PhysicsAndCollisionsJonathan/main.lua
+++ PhysicsAndCollisionsJonathan/main.lua
@@ -39,8 +39,46 @@ local ground = display.newImageRect("Images/ground.png", 5008, 50)
  -- send it to the back layer
  beam:toBack()
 
+
+local longBeam = display.newImageRect("Images/longBeam.png", 1008, 50)
+ longBeam.x = 1000
+ longBeam.y = 470
+
+ physics.addBody(longBeam, "static", {friction = 0.5, bounce = 0.3})
+
+
+ -- rotate the bame -60 degrees so its on an angle
+ longBeam:rotate(90)
+
  -- add to physics
  physics.addBody(beam, "static", {friction = 0.5, bounce = 0.3})
+
+local longBeam = display.newImageRect("Images/longBeam.png", 1500, 50)
+ longBeam.x = 1000
+ longBeam.y = 100
+
+ physics.addBody(longBeam, "static", {friction = 0.5, bounce = 0.3})
+
+
+ -- rotate the bame -60 degrees so its on an angle
+ longBeam:rotate(180)
+
+ -- add to physics
+ physics.addBody(longBeam, "static", {friction = 0.5, bounce = 0.3})
+
+local longBeam2 = display.newImageRect("Images/longBeam2.png", 1008, 50)
+ longBeam2.x = 25
+ longBeam2.y = 100
+
+ physics.addBody(longBeam2, "static", {friction = 0.5, bounce = 0.3})
+
+
+ -- rotate the bame -60 degrees so its on an angle
+ longBeam2:rotate(90)
+
+ -- add to physics
+ physics.addBody(longBeam2, "static", {friction = 0.5, bounce = 0.3})
+
 
 -- set the x and y position
 local bkg = display.newImageRect("Images/bkg.png", 2049, 1593)
@@ -56,5 +94,58 @@ bkg:toBack()
 
 -- create the first ball
 local function firstBall()
-	-- creating first ball
-	local ball
+	
+	local superBall1 = display.newImage("Images/superBall1.png", 100, 100)
+
+	-- add to physics
+	physics.addBody(superBall1, {density = 10.0, friction = 0.5, bounce = 0.9, radius = 10})
+
+	superBall1:scale( 1, 1 )
+end
+
+---------------------------------------------------------------------
+
+local function secondBall()
+	
+	local superBall2 = display.newImage("Images/superBall2.png", 300, 100)
+
+    -- add to physics
+	physics.addBody(superBall2, {density = 10.0, friction = 4.5, bounce = 0.5, radius = 25})
+
+    superBall2:scale( 2, 2)
+end
+
+--------------------------------------------------------------------
+
+-- create the first ball
+local function thirdBall() 
+	
+	local superBall3 = display.newImage("Images/superBall3.png", 250, 100)
+
+	-- add to physics
+	physics.addBody(superBall3, {density = 1.0, friction = 0.5, bounce = 0.5, radius = 65})
+
+	superBall3:scale( 3, 3)
+end
+
+-----------------------------------------------------------------------
+
+local function fourthBall()
+	
+	local superBall4 = display.newImage("Images/superBall4.png", 300, 100)
+
+    -- add to physics
+	physics.addBody(superBall4, {density = 10.0, friction = 0.5, bounce = 0.5, radius = 65})
+
+    superBall4:scale( 4, 4)
+end
+
+
+-----------------------------------------------------------------------
+-- TIMER DELAYS
+-----------------------------------------------------------------------
+
+timer.performWithDelay( 0, firstBall)
+timer.performWithDelay( 500, secondBall)
+timer.performWithDelay( 700, thirdBall)
+timer.performWithDelay( 900, fourthBall)
